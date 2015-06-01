e2aa79e74b0152f9ad2e83ba7a9dc49bbad9833d
evertmbh <evertmbh@hotmail.com>
2015-06-01 02:08:09 -0300

    Try to make connection sqLite JDBC

--- sudbrain/src/br/com/project/sudbrain/model/dao/ConnectionFactoryDAO.java
+++ sudbrain/src/br/com/project/sudbrain/model/dao/ConnectionFactoryDAO.java
@@ -8,18 +8,20 @@ import java.sql.*;
 
 public class ConnectionFactoryDAO {
 	
+	private final static String DRIVER_SQLITE = "org.sqlite.JDBC";
+	private final static String URL_SQLITE = "jdbc:sqlite:/home/everton/git/projetos/sudbrain/database/sudbraindb";	
 	
-	private final static String DRIVER = "org.postgresql.Driver";
-	private final static String URL = "jdbc:postgresql://localhost:5432/sudbraindb";
-	private static String userName = "postgres";
-	private static String password = "everton";
+//	private final static String DRIVER = "org.postgresql.Driver";
+//	private final static String URL = "jdbc:postgresql://localhost:5432/sudbraindb";
+//	private static String userName = "postgres";
+//	private static String password = "everton";
 	
 	public static Connection criaConexao(){
 		Connection con = null;
 		try{
-			Class.forName(DRIVER).newInstance();
-			con = DriverManager.getConnection(URL, userName, password);
-			System.out.println("Conexao criada com sucesso");
+			Class.forName(DRIVER_SQLITE).newInstance();
+			con = DriverManager.getConnection(URL_SQLITE);
+			System.out.println("Conexao criada com sucesso");	
 		}catch(Exception e){
 			System.out.println("Erro ao criar conexao");
 			e.printStackTrace();
